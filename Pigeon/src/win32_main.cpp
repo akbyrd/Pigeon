aa4ca3ac9477389f39da114ab0b7eebe4e257157
Adam Byrd <adam1.byrd@gmail.com>
2017-03-05 18:24:24 -0600

    Settled on an error handling pattern

--- Pigeon/src/win32_main.cpp
+++ Pigeon/src/win32_main.cpp
@@ -13,117 +13,13 @@ static const c16* PIGEON_GUID = L"{C1FA11EF-FC16-46DF-A268-104F59E94672}";
 static const c16* SINGLE_INSTANACE_MUTEX_NAME = L"Pigeon Single Instance Mutex";
 static const c16* NEW_PROCESS_MESSAGE_NAME = L"Pigeon New Process Name";
 
-auto
-CreateNotificationWindow = [](NotificationWindow& notification, HINSTANCE hInstance)
+enum struct InitPhase
 {
-	WNDCLASSW windowClass = {};
-	windowClass.style         = 0; //CS_DROPSHADOW
-	windowClass.lpfnWndProc   = NotificationWndProc;
-	windowClass.cbClsExtra    = 0;
-	windowClass.cbWndExtra    = sizeof(&notification);
-	windowClass.hInstance     = hInstance;
-	windowClass.hIcon         = nullptr;
-	windowClass.hCursor       = nullptr;
-	windowClass.hbrBackground = nullptr;
-	windowClass.lpszMenuName  = nullptr;
-	windowClass.lpszClassName = L"Pigeon Notification Class";
-
-	ATOM classAtom = RegisterClassW(&windowClass);
-	if (classAtom == INVALID_ATOM)
-	{
-		NotifyWindowsError(&notification, L"RegisterClassW failed");
-		return false;
-	}
-
-	notification.hwnd = CreateWindowExW(
-		WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_NOACTIVATE | WS_EX_TRANSPARENT,
-		windowClass.lpszClassName,
-		L"Pigeon Notification Window",
-		WS_POPUP,
-		notification.windowPosition.x,
-		notification.windowPosition.y,
-		notification.windowSize.cx,
-		notification.windowSize.cy,
-		nullptr,
-		nullptr,
-		hInstance,
-		&notification
-	);
-	if (notification.hwnd == INVALID_HANDLE_VALUE)
-	{
-		notification.hwnd = nullptr;
-		NotifyWindowsError(&notification, L"CreateWindowExW failed");
-		return false;
-	}
-
-	return true;
-};
-
-/* TODO: Still have some failure cases
-* - If an older process reaches the wait after a newer process it's going to get stuck there.
-* - If a newer process posts a message before an older process creates its window and the
-*   older process acquires the mutex from an even older process it will hang (until a new
-*   message is posted)
-*/
-auto
-EnforceSingleInstance = [](NotificationWindow& notification, u64& startTime, u32& WM_NEWINSTANCE, HANDLE& singleInstanceMutex)
-{
-	HANDLE hProcess = GetCurrentProcess();
-
-	FILETIME win32_startFileTime = {};
-	FILETIME unusued;
-	b32 success = GetProcessTimes(hProcess, &win32_startFileTime, &unusued, &unusued, &unusued);
-	if (!success)
-	{
-		NotifyWindowsError(&notification, L"GetProcessTimes failed");
-		return false;
-	}
-
-	ULARGE_INTEGER win32_startTime = {};
-	win32_startTime.LowPart  = win32_startFileTime.dwLowDateTime;
-	win32_startTime.HighPart = win32_startFileTime.dwHighDateTime;
-
-	startTime = win32_startTime.QuadPart;
-
-	WM_NEWINSTANCE = RegisterWindowMessageW(NEW_PROCESS_MESSAGE_NAME);
-	if (WM_NEWINSTANCE == 0)
-	{
-		NotifyWindowsError(&notification, L"RegisterWindowMessage failed");
-		return false;
-	}
-
-	// TOOD: Namespace?
-	singleInstanceMutex = CreateMutexW(nullptr, true, SINGLE_INSTANACE_MUTEX_NAME);
-	if (!singleInstanceMutex)
-	{
-		NotifyWindowsError(&notification, L"CreateMutex failed");
-		return false;
-	}
-
-	if (GetLastError() == ERROR_ALREADY_EXISTS)
-	{
-		// TODO: Better to enumerate processes instead of broadcasting
-		success = PostMessageW(HWND_BROADCAST, WM_NEWINSTANCE, startTime, 0);
-		if (!success)
-		{
-			NotifyWindowsError(&notification, L"PostMessage failed");
-			return false;
-		}
-
-		// TODO: Not handling messages while waiting
-		u32 uResult = WaitForSingleObject(singleInstanceMutex, INFINITE);
-		if (uResult == WAIT_FAILED)
-		{
-			singleInstanceMutex = nullptr;
-
-			NotifyWindowsError(&notification, L"WaitForSingleObject WAIT_FAILED");
-			return false;
-		}
-
-		if (uResult == WAIT_ABANDONED) NotifyWindowsError(&notification, L"WaitForSingleObject WAIT_ABANDON", Error::Warning);
-	}
-
-	return true;
+	None,
+	WindowCreated,
+	SingleInstanceEnforced,
+	HotkeysRegistered,
+	SystemsInitialized,
 };
 
 struct Hotkey
@@ -136,24 +32,161 @@ struct Hotkey
 	b32 registered = false;
 };
 
-b32 UnregisterHotkeys(NotificationWindow&, Hotkey*, u8, HANDLE);
-auto
-RegisterHotkeys = [](NotificationWindow& notification, Hotkey* hotkeys, u8 hotkeyCount, HANDLE singleInstanceMutex)
+b32
+Initialize(InitPhase& phase,
+           NotificationWindow& notification, HINSTANCE hInstance,
+           u64& startTime, u32& WM_NEWINSTANCE, HANDLE& singleInstanceMutex,
+           Hotkey* hotkeys, u8 hotkeyCount)
 {
-	for (u8 i = 0; i < hotkeyCount; i++)
+	//Create window
 	{
-		b32 success = RegisterHotKey(nullptr, hotkeys[i].id, MOD_WIN | MOD_NOREPEAT | hotkeys[i].modifier, hotkeys[i].key);
-		if (!success)
+		WNDCLASSW windowClass = {};
+		windowClass.style         = 0; //CS_DROPSHADOW
+		windowClass.lpfnWndProc   = NotificationWndProc;
+		windowClass.cbClsExtra    = 0;
+		windowClass.cbWndExtra    = sizeof(&notification);
+		windowClass.hInstance     = hInstance;
+		windowClass.hIcon         = nullptr;
+		windowClass.hCursor       = nullptr;
+		windowClass.hbrBackground = nullptr;
+		windowClass.lpszMenuName  = nullptr;
+		windowClass.lpszClassName = L"Pigeon Notification Class";
+
+		ATOM classAtom = RegisterClassW(&windowClass);
+		if (classAtom == INVALID_ATOM)
 		{
-			NotifyWindowsError(&notification, L"RegisterHotKey failed");
-			UnregisterHotkeys(notification, hotkeys, hotkeyCount, singleInstanceMutex);
+			NotifyWindowsError(&notification, L"RegisterClassW failed");
 			return false;
 		}
+
+		notification.hwnd = CreateWindowExW(
+			WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_NOACTIVATE | WS_EX_TRANSPARENT,
+			windowClass.lpszClassName,
+			L"Pigeon Notification Window",
+			WS_POPUP,
+			notification.windowPosition.x,
+			notification.windowPosition.y,
+			notification.windowSize.cx,
+			notification.windowSize.cy,
+			nullptr,
+			nullptr,
+			hInstance,
+			&notification
+		);
+		if (notification.hwnd == INVALID_HANDLE_VALUE)
+		{
+			notification.hwnd = nullptr;
+			NotifyWindowsError(&notification, L"CreateWindowExW failed");
+			return false;
+		}
+
+		phase = InitPhase::WindowCreated;
 	}
-};
+
+
+	//Enforce single instance
+	{
+		/* TODO: Still have some failure cases
+		* - If an older process reaches the wait after a newer process it's going to get stuck there.
+		* - If a newer process posts a message before an older process creates its window and the
+		*   older process acquires the mutex from an even older process it will hang (until a new
+		*   message is posted)
+		*/
+
+		HANDLE hProcess = GetCurrentProcess();
+
+		FILETIME win32_startFileTime = {};
+		FILETIME unusued;
+		b32 success = GetProcessTimes(hProcess, &win32_startFileTime, &unusued, &unusued, &unusued);
+		if (!success)
+		{
+			NotifyWindowsError(&notification, L"GetProcessTimes failed");
+			return false;
+		}
+
+		ULARGE_INTEGER win32_startTime = {};
+		win32_startTime.LowPart  = win32_startFileTime.dwLowDateTime;
+		win32_startTime.HighPart = win32_startFileTime.dwHighDateTime;
+
+		startTime = win32_startTime.QuadPart;
+
+		WM_NEWINSTANCE = RegisterWindowMessageW(NEW_PROCESS_MESSAGE_NAME);
+		if (WM_NEWINSTANCE == 0)
+		{
+			NotifyWindowsError(&notification, L"RegisterWindowMessage failed");
+			return false;
+		}
+
+		// TOOD: Namespace?
+		singleInstanceMutex = CreateMutexW(nullptr, true, SINGLE_INSTANACE_MUTEX_NAME);
+		if (!singleInstanceMutex)
+		{
+			NotifyWindowsError(&notification, L"CreateMutex failed");
+			return false;
+		}
+
+		if (GetLastError() == ERROR_ALREADY_EXISTS)
+		{
+			// TODO: Better to enumerate processes instead of broadcasting
+			success = PostMessageW(HWND_BROADCAST, WM_NEWINSTANCE, startTime, 0);
+			if (!success)
+			{
+				NotifyWindowsError(&notification, L"PostMessage failed");
+				return false;
+			}
+
+			// TODO: Not handling messages while waiting
+			u32 uResult = WaitForSingleObject(singleInstanceMutex, INFINITE);
+			if (uResult == WAIT_FAILED)
+			{
+				singleInstanceMutex = nullptr;
+
+				NotifyWindowsError(&notification, L"WaitForSingleObject WAIT_FAILED");
+				return false;
+			}
+
+			if (uResult == WAIT_ABANDONED) NotifyWindowsError(&notification, L"WaitForSingleObject WAIT_ABANDON", Error::Warning);
+		}
+
+		phase = InitPhase::SingleInstanceEnforced;
+	}
+
+
+	//Register hotkeys
+	{
+		for (u8 i = 0; i < hotkeyCount; i++)
+		{
+			b32 success = RegisterHotKey(nullptr, hotkeys[i].id, MOD_WIN | MOD_NOREPEAT | hotkeys[i].modifier, hotkeys[i].key);
+			if (!success)
+			{
+				NotifyWindowsError(&notification, L"RegisterHotKey failed");
+
+				b32 UnregisterHotkeys(NotificationWindow&, Hotkey*, u8, HANDLE);
+				success = UnregisterHotkeys(notification, hotkeys, hotkeyCount, singleInstanceMutex);
+				if (!success) phase = InitPhase::HotkeysRegistered;
+
+				return false;
+			}
+		}
+
+		phase = InitPhase::HotkeysRegistered;
+	}
+
+
+	//Initialize systems
+	{
+		b32 success = InitializeAudio(&notification);
+		if (!success) return false;
+
+		phase = InitPhase::SystemsInitialized;
+	}
+
+
+	return true;
+}
 
 b32
-UnregisterHotkeys(NotificationWindow& notification, Hotkey* hotkeys, u8 hotkeyCount, HANDLE singleInstanceMutex)
+UnregisterHotkeys(NotificationWindow& notification, Hotkey* hotkeys, u8 hotkeyCount, HANDLE& singleInstanceMutex)
 {
 	bool unregisterFailed = false;
 	for (u8 i = 0; i < hotkeyCount; i++)
@@ -186,124 +219,11 @@ UnregisterHotkeys(NotificationWindow& notification, Hotkey* hotkeys, u8 hotkeyCo
 	return true;
 };
 
-auto
-Misc = [](NotificationWindow& notification)
-{
-	b32 success = SetPriorityClass(GetCurrentProcess(), BELOW_NORMAL_PRIORITY_CLASS);
-	if (!success) NotifyWindowsError(&notification, L"SetPriorityClass failed", Error::Warning);
-};
-
-
-auto
-MessageLoop = [](NotificationWindow& notification, u64 startTime, u32 WM_NEWINSTANCE, Hotkey* hotkeys, u8 hotkeyCount, HANDLE singleInstanceMutex)
-{
-	MSG msg = {};
-	b32 quit = false;
-
-	while (!quit)
-	{
-		i32 iResult = GetMessageW(&msg, nullptr, 0, 0);
-		if (iResult == -1)
-		{
-			// TODO: Log error
-			break;
-		}
-
-		// TODO: Handle return values?
-		TranslateMessage(&msg);
-		DispatchMessageW(&msg);
-
-		if (msg.message == WM_NEWINSTANCE)
-		{
-			u64 newProcessStartTime = msg.wParam;
-
-			// TODO: This will lead to a hang if 2 processes have the exact same start time (include Process/ThreadID, highest wins?)
-			if (newProcessStartTime > startTime)
-			{
-				if (singleInstanceMutex)
-				{
-					UnregisterHotkeys(notification, hotkeys, hotkeyCount, singleInstanceMutex);
-				}
-
-				notification.windowPosition.y += notification.windowSize.cy + 10;
-				UpdateWindowPositionAndSize(&notification);
-
-				Notify(&notification, L"There can be only one!", Error::Error);
-			}
-		}
-		else
-		{
-			switch (msg.message)
-			{
-			case WM_HOTKEY:
-			{
-				for (u8 i = 0; i < hotkeyCount; i++)
-				{
-					if (msg.wParam == hotkeys[i].id)
-					{
-						hotkeys[i].execute(&notification);
-						break;
-					}
-				}
-				break;
-			}
-
-			case WM_QUIT:
-				quit = true;
-				break;
-
-				// Expected messages
-			case WM_TIMER:
-			case WM_PROCESSQUEUE:
-				break;
-
-				// TODO: WM_FONTCHANGE (29) - when installing fonts
-				// TODO: WM_TIMECHANGE (30) - Shows up somewhat randomly
-				// TODO: WM_KEYDOWN/UP (256, 257) - Somehow we're getting key messages, but only sometimes
-			default:
-				if (msg.message < WM_PROCESSQUEUE)
-					NotifyFormat(&notification, L"Unexpected message: %d\n", Error::Warning, msg.message);
-				break;
-			}
-		}
-	}
-
-	// TODO: This is wrong
-	return LOWORD(msg.wParam);
-};
 
 int CALLBACK
 wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, i32 nCmdShow)
 {
-	/* TODO: Overhaul error handling
-	 * General policy: if the program runs, it will have full functionality
-	 * 
-	 * - Option 1: Nested ifs, inline
-	 *    Pros: 'simple'
-	 *    Cons: Harder to read, separates error location and handling
-	 * 
-	 * - Option 2: Nested ifs, functions
-	 *    Pros: Keeps error location somewhat closer to occurrence location
-	 *    Cons: Harder to read, jumping back and forth through (use-once) functions
-	 * 
-	 * - Option 3: State booleans
-	 *    Pros: Keeps code linear
-	 *    Cons: Maybe harder to follow? Combinatorial state to keep in head
-	 * 
-	 * - Option 4: Stage array or boolean
-	 *    Pros: Keeps code linear; non-combinatorial
-	 *    Cons: Slightly less obvious than nested ifs, still need nested ifs for smaller steps
-	 *          (or ending up with a ton of enum values [probably too much overhead])
-	 *    Impl: loop over enum values, switch, check index upon completion?
-	 *    Impl: Check enum before each block, set enum when done
-	 * 
-	 * - Option 4: Local functions!
-	 *    Pros: Keeps code linear, no large jumps in code, no nested ifs at all
-	 *    Cons: Boilerplate code, stepping through with the debugger is a bit unintuitive
-	 *    Impl: lambda functions
-	 *    Impl: Compare the generated assembly for a lambdas a regular function in a sandbox project
-	 *    Impl: Auto-capture by reference? (Does this fuck up the generated code?)
-	 */
+	// TODO: Finish refactoring error handling.
 
 	// TODO: Show warning, show another warning while first is hiding => shows 2 warnings (repeating the first?)
 	// TODO: Hotkeys don't work in fullscreen apps (e.g. Darksiders 2)
@@ -368,29 +288,106 @@ wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, i32 nCmdS
 	};
 
 
-	int returnValue = -1;
-	if (CreateNotificationWindow(notification, hInstance))
-	{
-		u64    startTime           = 0;
-		u32    WM_NEWINSTANCE      = 0;
-		HANDLE singleInstanceMutex = nullptr;
+	//Initialize
+	InitPhase phase = InitPhase::None;
 
-		if (EnforceSingleInstance(notification, startTime, WM_NEWINSTANCE, singleInstanceMutex))
+	u64    startTime           = 0;
+	u32    WM_NEWINSTANCE      = 0;
+	HANDLE singleInstanceMutex = nullptr;
+
+	Initialize(phase,
+		notification, hInstance,
+		startTime, WM_NEWINSTANCE, singleInstanceMutex,
+		hotkeys, ArrayCount(hotkeys)
+	);
+
+
+	//Misc
+	b32 success = SetPriorityClass(GetCurrentProcess(), BELOW_NORMAL_PRIORITY_CLASS);
+	if (!success) NotifyWindowsError(&notification, L"SetPriorityClass failed", Error::Warning);
+
+
+	//Message loop
+	int returnValue = -1;
+	{
+		MSG msg = {};
+		b32 quit = false;
+
+		while (!quit)
 		{
-			if (RegisterHotkeys(notification, hotkeys, ArrayCount(hotkeys), singleInstanceMutex))
+			i32 iResult = GetMessageW(&msg, nullptr, 0, 0);
+			if (iResult == -1)
 			{
-				if (InitializeAudio(&notification))
+				// TODO: Log error
+				break;
+			}
+
+			// TODO: Handle return values?
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+
+			if (msg.message == WM_NEWINSTANCE)
+			{
+				u64 newProcessStartTime = msg.wParam;
+
+				// TODO: This will lead to a hang if 2 processes have the exact same start time (include Process/ThreadID, highest wins?)
+				if (newProcessStartTime > startTime)
 				{
-					Misc(notification);
-					returnValue = MessageLoop(notification, startTime, WM_NEWINSTANCE, hotkeys, ArrayCount(hotkeys), singleInstanceMutex);
+					if (singleInstanceMutex)
+					{
+						UnregisterHotkeys(notification, hotkeys, ArrayCount(hotkeys), singleInstanceMutex);
+					}
+
+					notification.windowPosition.y += notification.windowSize.cy + 10;
+					UpdateWindowPositionAndSize(&notification);
+
+					Notify(&notification, L"There can be only one!", Error::Error);
 				}
-				TeardownAudio();
+			}
+			else
+			{
+				switch (msg.message)
+				{
+				case WM_HOTKEY:
+				{
+					for (u8 i = 0; i < ArrayCount(hotkeys); i++)
+					{
+						if (msg.wParam == hotkeys[i].id)
+						{
+							hotkeys[i].execute(&notification);
+							break;
+						}
+					}
+					break;
+				}
+
+				case WM_QUIT:
+					quit = true;
+					// TODO: This is wrong
+					returnValue = LOWORD(msg.wParam);
+					break;
+
+					// Expected messages
+				case WM_TIMER:
+				case WM_PROCESSQUEUE:
+					break;
+
+					// TODO: WM_FONTCHANGE (29) - when installing fonts
+					// TODO: WM_TIMECHANGE (30) - Shows up somewhat randomly
+					// TODO: WM_KEYDOWN/UP (256, 257) - Somehow we're getting key messages, but only sometimes
+				default:
+					if (msg.message < WM_PROCESSQUEUE)
+						NotifyFormat(&notification, L"Unexpected message: %d\n", Error::Warning, msg.message);
+					break;
+				}
 			}
 		}
 	}
 
 
-	// Shutdown
+	// Cleanup
+	if (phase >= InitPhase::SystemsInitialized)
+		TeardownAudio();
 
 	// TODO: Show remaining warnings / errors in a dialog?
 
